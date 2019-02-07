1e54bdee061b40275d4ab9cbae1e13e2d14b6ab1
Jason Tung <jasontung123@yahoo.com>
2019-02-07 00:15:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,3 @@
-def 'Deli Counter' do
-
-  let(:katz_deli) { [] }
-  let(:other_deli) { ["Logan", "Avi", "Spencer"] }
-  let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
-
 def line 
   puts "The line is currently empty."
 end
